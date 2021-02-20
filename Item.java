6e4910bca8f693cf9892d3a721355bce8cd35adf
overlordcouch <mark.ansellrose@wsu.edu>
2021-02-20 14:09:30 -0800

    Created Item superclass.  Added state fields.

---
+++ Item.java
@@ -0,0 +1,24 @@
+
+/**
+ * Item superclass contains state and methods common to all items
+ */
+public class Item {
+	
+	/**
+	 * The level of the item.
+	 */
+	 private int level;
+	 
+	 /**
+	  * All items will have a general type description.  For example, a
+	  * Weapon will be of type sword, staff, etc.
+	  */
+	 private String type;
+	 
+	 /**
+	  * All items will have descriptive words associated with them.
+	  */
+	 private String description;
+	
+	
+}//item super class
