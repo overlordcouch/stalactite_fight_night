ce01cfd761693c28fd773bcf6423e7c99e78bd27
overlordcouch <mark.ansellrose@wsu.edu>
2021-02-20 23:53:09 -0800

    Changed TestMain to instantiate from driver class

--- TestMain.java
+++ TestMain.java
@@ -6,23 +6,9 @@ public class TestMain{
 	
 	public static void main(String[] args){
 		
-		try{
-			File file = new File("dev_documents/splash.txt");
-			
-			Scanner reader = new Scanner(file);
-			
-			while(reader.hasNextLine()){
-				System.out.println(reader.nextLine());
-			}
-		}catch(Exception FileNotFoundException){
-			/*If the chosen file name is not valid, this message will print
-			 * to the terminal.*/
-			System.out.println("SPlash file not found in current directory.\n");
-		}
+		StalactiteFightNight game = new StalactiteFightNight();
 		
-		System.out.println("\t\t\t\t\t\t\t\t\t\t  Press 's' to start a new game");
-		System.out.println("\t\t\t\t\t\t\t\t\t\t                 or            ");
-		System.out.println("\t\t\t\t\t\t\t\t\t\t  Press 'i' for some instructions");
+		game.splashPrint();
 		
 		
 		
