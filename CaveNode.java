746011e647db1cc3261c4f7f714df2a0b72361f9
overlordcouch <mark.ansellrose@wsu.edu>
2021-02-21 20:18:45 -0800

    Added CaveNode module skeleton.

---
+++ CaveNode.java
@@ -0,0 +1,11 @@
+/**
+ * CaveNode class is the building block of the cave adventure.  Each
+ * room will consist of a CaveNode that contains the pertinent information
+ * about the room's contents and description.
+ * 
+ * @author M.Ansell
+ * @version 1.0
+ */
+public class CaveNode{
+	
+}//CaveNode class
