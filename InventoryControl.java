932f30acce5cde359149ba086b1b4414a4388677
overlordcouch <mark.ansellrose@wsu.edu>
2021-03-04 12:55:44 -0800

    Created inventoryMain, beginning work on interactions

--- InventoryControl.java
+++ InventoryControl.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 
 /**
  * InventoryControl class is the main state driver for player inventory
@@ -8,5 +8,23 @@
  * @version 1.0
  */
 public class InventoryControl{
+	
+	/**
+	 * Local pointer to the player's inventory.
+	 * 
+	 * @since 1.0
+	 */
+	private static List<Item> inventory = StalactiteFightNight.player.getInventory();
+	
+	
+	/**
+	 * The foundational state function for inventory interactions by the 
+	 * player.
+	 * 
+	 * @since 1.0
+	 */
+	public static void inventoryMain(){
+		
+	}//inventoryMain
 	
 }//InventoryControl Class
