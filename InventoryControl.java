6f9e0722fa673db62e92158a74d48f926b85d40c
overlordcouch <mark.ansellrose@wsu.edu>
2021-03-04 13:01:12 -0800

    Added intial operations of inventory state, working on printing inventory.

--- InventoryControl.java
+++ InventoryControl.java
@@ -16,6 +16,13 @@ public class InventoryControl{
 	 */
 	private static List<Item> inventory = StalactiteFightNight.player.getInventory();
 	
+	/**
+	 * Local pointer to the player object.
+	 * 
+	 * @since 1.0
+	 */
+	private static Adventurer player = StalactiteFightNight.player;
+	
 	
 	/**
 	 * The foundational state function for inventory interactions by the 
@@ -25,6 +32,22 @@ public class InventoryControl{
 	 */
 	public static void inventoryMain(){
 		
+		/*Clear screen and print player information*/
+		Helper.clearWindow();
+		Helper.clearInputBuffer();
+		Helper.printPlayerHeader();
+		
+		printInventory();
+		
 	}//inventoryMain
+	
+	/**
+	 * Prints a formatted version of the player's inventory to the terminal.
+	 * 
+	 * @since 1.0
+	 */
+	private static void printInventory(){
+		
+	}
 	
 }//InventoryControl Class
